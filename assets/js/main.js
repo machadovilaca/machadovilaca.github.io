4c28c5b7b51597db66a30820a902cd01a5bce4a9
João Vilaça <machadovilaca@gmail.com>
2019-06-24 01:55:08 +0100

    Initial Website

---
+++ assets/js/main.js
@@ -0,0 +1,101 @@
+(function($) {
+
+  "use strict";
+
+  $(window).on('load', function() {
+    var ageDifMs = Date.now() - new Date("1998-06-30").getTime();
+    var ageDate = new Date(ageDifMs);
+    $("#age").text(Math.abs(ageDate.getUTCFullYear() - 1970) + " Years");
+
+    var experienceDifMs = Date.now() - new Date("2017-06-01").getTime();
+    var experienceDate = new Date(experienceDifMs);
+    $("#experience").text(Math.abs(experienceDate.getUTCFullYear() - 1970) + " Years");
+
+    /*
+   MixitUp
+   ========================================================================== */
+  $('#portfolio').mixItUp();
+
+  /*
+   One Page Navigation & wow js
+   ========================================================================== */
+    var OnePNav = $('.onepage-nev');
+    var top_offset = OnePNav.height() - -0;
+    OnePNav.onePageNav({
+      currentClass: 'active',
+      scrollOffset: top_offset,
+    });
+
+  /*Page Loader active
+    ========================================================*/
+    $('#preloader').fadeOut();
+
+  // Sticky Nav
+    $(window).on('scroll', function() {
+        if ($(window).scrollTop() > 200) {
+            $('.scrolling-navbar').addClass('top-nav-collapse');
+        } else {
+            $('.scrolling-navbar').removeClass('top-nav-collapse');
+        }
+    });
+
+    /* slicknav mobile menu active  */
+    $('.mobile-menu').slicknav({
+        prependTo: '.navbar-header',
+        parentTag: 'liner',
+        allowParentLinks: true,
+        duplicate: true,
+        label: '',
+        closedSymbol: '<i class="icon-arrow-right"></i>',
+        openedSymbol: '<i class="icon-arrow-down"></i>',
+      });
+
+      /* WOW Scroll Spy
+    ========================================================*/
+     var wow = new WOW({
+      //disabled for mobile
+        mobile: false
+    });
+
+    wow.init();
+
+    /* Nivo Lightbox
+    ========================================================*/
+    $('.lightbox').nivoLightbox({
+        effect: 'fadeScale',
+        keyboardNav: true,
+      });
+
+    /* Counter
+    ========================================================*/
+    $('.counterUp').counterUp({
+     delay: 10,
+     time: 1000
+    });
+
+
+    /* Back Top Link active
+    ========================================================*/
+      var offset = 200;
+      var duration = 500;
+      $(window).scroll(function() {
+        if ($(this).scrollTop() > offset) {
+          $('.back-to-top').fadeIn(400);
+        } else {
+          $('.back-to-top').fadeOut(400);
+        }
+      });
+
+      $('.back-to-top').on('click',function(event) {
+        event.preventDefault();
+        $('html, body').animate({
+          scrollTop: 0
+        }, 600);
+        return false;
+      });
+
+
+
+  });
+
+}(jQuery));
